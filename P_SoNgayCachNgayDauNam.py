06c4d8fd00f1308e6851b00cc77a7b9c17aea384
TamNhien <canhthien91@gmail.com>
2024-03-21 21:15:53 +0700

    Add project files.

---
+++ P_SoNgayCachNgayDauNam.py
@@ -0,0 +1,8 @@
+﻿ngay = int(input("Nhập ngày : "))
+thang = int(input("Nhập tháng : "))
+
+so_ngay_trong_thang = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  # Số ngày trong từng tháng của năm không nhuận
+
+so_ngay = sum(so_ngay_trong_thang[:thang - 1]) + ngay  # Tính số ngày từ đầu năm đến ngày nhập vào
+
+print("Ngày", ngay, "tháng", thang, "cách ngày đầu năm", so_ngay, "ngày")  # In ra kết quả
